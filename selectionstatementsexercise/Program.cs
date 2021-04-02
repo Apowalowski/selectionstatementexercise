ce28f4d45441b6570a1366a4b53c679de25c852f
Apowalowski <acpowalowski@gmail.com>
2021-04-02 15:56:57 -0400

    selectionstatementexercise

---
+++ selectionstatementsexercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace selectionstatementsexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try to guess my favorite number");
+            int x = int.Parse(Console.ReadLine());
+            if (x == 25)
+            {
+                Console.WriteLine("You are right!");
+            }
+            else if (x > 25)
+            {
+                Console.WriteLine("Too High.");
+            }
+            else 
+            {
+                Console.WriteLine("Too Low.");
+            }
+               
+
+        }
+    }
+}
